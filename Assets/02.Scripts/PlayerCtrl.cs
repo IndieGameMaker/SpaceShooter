5d5fadd2a5800600e7d825781e8fd5564c50967c
Lee Jae Hyun <myleje@gmail.com>
2021-07-24 14:44:45 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -21,8 +21,9 @@ public class PlayerCtrl : MonoBehaviour
         transform.Translate(Vector3.forward * 0.1f);
 
         h = Input.GetAxis("Horizontal"); // -1.0f ~ 0.0f ~ +1.0f
+        v = Input.GetAxis("Vertical");   // -1.0f ~ 0.0f ~ +1.0f
 
-        Debug.Log("h=" + h);
+        Debug.Log("h=" + h + "v=" + v);
     }
 
 

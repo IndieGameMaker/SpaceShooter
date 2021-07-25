95476937274d6eb93ba450902ac3a84dab7d579e
Lee Jae Hyun <myleje@gmail.com>
2021-07-25 11:40:30 +0900

    .

--- Assets/02.Scripts/BarrelCtrl.cs
+++ Assets/02.Scripts/BarrelCtrl.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BarrelCtrl : MonoBehaviour
 {
@@ -8,10 +10,18 @@ public class BarrelCtrl : MonoBehaviour
     public Texture[] textures;
     public new MeshRenderer renderer;
 
-    // Start is called before the first frame update
+    /*
+        난수 발생
+        Random.Range(min, max)
+
+        Random.Range(0, 10)  => 0,1,2,...,9
+        Random.Range(0.0f, 10.0f)   => 0.0f, ~ , 10.0f
+    */
     void Start()
     {
+        int idx = Random.Range(0, textures.Length); // 0, 1, 2
         renderer = GetComponentInChildren<MeshRenderer>();
+        renderer.material.mainTexture = textures[idx];
     }
 
     void OnCollisionEnter(Collision coll)

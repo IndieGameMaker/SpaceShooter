24943d407b7004792affe9285426781b9684cd9a
Lee Jae Hyun <myleje@gmail.com>
2021-07-25 11:30:07 +0900

    .

--- Assets/02.Scripts/BarrelCtrl.cs
+++ Assets/02.Scripts/BarrelCtrl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BarrelCtrl : MonoBehaviour
 {
     private int hitCount;
+    public Texture[] textures;
 
     // Start is called before the first frame update
     void Start()

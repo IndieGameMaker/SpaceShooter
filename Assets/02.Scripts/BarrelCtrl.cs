68bbb2d7bf4f92ca41f53799f3725c748bde48a5
Lee Jae Hyun <myleje@gmail.com>
2021-07-25 11:23:02 +0900

    .

--- Assets/02.Scripts/BarrelCtrl.cs
+++ Assets/02.Scripts/BarrelCtrl.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class BarrelCtrl : MonoBehaviour
 {
+    private int hitCount;
+
     // Start is called before the first frame update
     void Start()
     {
 
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision coll)
     {
+        if (coll.collider.CompareTag("BULLET"))
+        {
+            if (++hitCount == 3)
+            {
+                ExpBarrel();
+            }
+        }
+    }
 
+    void ExpBarrel()
+    {
+        Rigidbody rb = this.gameObject.AddComponent<Rigidbody>();
+        rb.AddForce(Vector3.up * 1500.0f);
+        Destroy(this.gameObject, 2.0f);
     }
 }

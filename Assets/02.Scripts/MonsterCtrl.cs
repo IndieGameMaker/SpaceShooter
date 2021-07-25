82c3d1474d84495fd3556819576af7bcd622a889
Lee Jae Hyun <myleje@gmail.com>
2021-07-25 17:26:24 +0900

    .

--- Assets/02.Scripts/MonsterCtrl.cs
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -9,10 +9,21 @@ public class MonsterCtrl : MonoBehaviour
         IDLE, TRACE, ATTACK, DIE
     }
 
-    // Start is called before the first frame update
+    // 몬스터의 상태값을 저장하기 위한 변수
+    public State state = State.IDLE;
+
+    private Transform playerTr; //주인공 캐릭터의 Transform 컴포넌트를 저장할 변수
+    private Transform monsterTr;
+
     void Start()
     {
+        // GameObject playerObj = GameObject.FindGameObjectWithTag("PLAYER");
+        // if (playerObj != null)
+        // {
+        //     playerTr = playerObj.GetComponent<Transform>();
+        // }
 
+        playerTr = GameObject.FindGameObjectWithTag("PLAYER")?.GetComponent<Transform>();
     }
 
     // Update is called once per frame

2ecba6de90ed55c8a50e657a8636fb475280df22
Lee Jae Hyun <myleje@gmail.com>
2021-07-25 09:40:52 +0900

    .

--- Assets/02.Scripts/RemoveBullet.cs
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -5,4 +5,13 @@ using UnityEngine;
 public class RemoveBullet : MonoBehaviour
 {
 
+    // 충돌 콜백함수(Call Back Function), 이벤트 함수(Event Function)
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.tag == "BULLET")
+        {
+            // 총알을 삭제
+            Destroy(coll.gameObject);
+        }
+    }
 }

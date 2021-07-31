0efcb47a0a76b3a9ddd19e59ec639ed21d7e2609
Lee Jae Hyun <myleje@gmail.com>
2021-07-31 15:47:09 +0900

    .

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -35,8 +35,24 @@ public class GameManager : MonoBehaviour
         ws = new WaitForSeconds(createTime);
 
         points = GameObject.Find("SpawnPointGroup")?.GetComponentsInChildren<Transform>();
+
+        CreatePooling();
         // InvokeRepeating("CreateMonster", 2.0f, createTime);
-        StartCoroutine(CreateMonster());
+        // StartCoroutine(CreateMonster());
+    }
+
+    void CreatePooling()
+    {
+        for (int i = 0; i < maxPool; i++)
+        {
+            // 몬스터 인스턴스 생성
+            GameObject monster = Instantiate<GameObject>(monsterPrefab);
+            monster.name = $"Monster_{i:00}";
+            monster.SetActive(false);
+
+            // 오브젝트 풀링에 추가
+            monsterPool.Add(monster);
+        }
     }
 
     IEnumerator CreateMonster()

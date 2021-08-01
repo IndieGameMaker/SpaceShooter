23732d1040b2ba48d5f9e80273e585b19b061a9a
Lee Jae Hyun <myleje@gmail.com>
2021-08-01 13:43:16 +0900

    .

--- Assets/02.Scripts/UIManager.cs
+++ Assets/02.Scripts/UIManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // 씬을 호출하기 위한 네임스페이스
 
 public class UIManager : MonoBehaviour
 {
     public void OnStartButtonClick()
     {
         Debug.Log("버튼 클릭");
+        SceneManager.LoadScene("Play");
     }
 }
